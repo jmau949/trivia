8113cd618296fc88fa71d13b25de54e2ba874c91
D Jonathan Mau <jmau@ucdavis.edu>
2019-07-18 02:19:23 -0500

    displayedquestions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,62 +17,62 @@ var options = [
     {
         question: "What is the name of the large red ball used in Quidditch?",
         choice: ["Quiddle", "Snitch", "Sniffle", "Quaffle"],
-        answer: "Griphook",
+        answer: 3,
         photo: "assets/images/quaffle.jpg",
     },
     {
         question: "What is the name of the goblin who assists Harry, Ron, and Hermione in infiltrating Gringotts?",
         choice: ["Gornuk", "Golgomath", "Griphook", "Gregorovitch"],
-        answer: "Griphook",
+        answer: 2,
         photo: "assets/images/griphook.jpg",
     },
     {
         question: "Which of the following dragons was NOT used in the Triwizard Tournament in 1994?",
         choice: ["Hungarian Horntail", "Swedish Short-Snout", "Norwegian Ridgeback", "Chinese Fireball"],
-        answer: "Norwegian Ridgeback",
+        answer: 3,
         photo: "assets/images/ridgeback.png",
     },
     {
         question: "What is the name of the Weasley family's elderly owl?",
         choice: ["Elphias", "Errol", "Ernie", "Eldred"],
-        answer: "Errol",
+        answer: 1,
         photo: "assets/images/errol.png",
     },
     {
         question: "Which of the Weasleys went to Romania to study dragons?",
         choice: ["George", "Bill", "Charlie", "Percy"],
-        answer: "Charlie",
+        answer: 2,
         photo: "assets/images/charlie.png",
 
     },
     {
         question: "Which is the house ghost of Hufflepuff?",
         choice: ["The Fat Friar", "The Grey Lady", "The Bloody Baron", "Nearly Headless Nick"],
-        answer: "The Fat Friar",
+        answer: 0,
         photo: "assets/images/friar.jpg",
     },
     {
         question: "Who was NOT a member of the 'Slug Club'?",
         choice: ["Cormac Mclaggen", "Hermione Granger", "Ron Weasley", "Harry Potter"],
-        answer: "Ron Weasley",
+        answer: 2,
         photo: "assets/images/ronald.png",
     },
     {
         question: "What kind of creature does Lee Jordan put in Professor Umbridge's office during the latter's reign as Hogwarts High Inquisitor?",
         choice: ["Norwegian Ridgeback", "Niffler", "Nargle", "Nymph"],
-        answer: "Niffler",
+        answer: 1,
         photo: "assets/images/niffler.png",
     },
     {
         question: "What profession are Hermione Granger's Muggle parents?",
         choice: ["Chefs", "Dentists", "Opticians", "Artists"],
-        answer: "Dentists",
+        answer: 2,
         photo: "assets/images/parents.jpg",
     },
     {
         question: "In which Quidditch position did Oliver Wood play when he was still at Hogwarts?",
         choice: ["Chaser", "Beater", "Keeper", "Seeker"],
-        answer: "Keeper",
+        answer: 3,
         photo: "assets/images/oliver.jpg",
     },
 ]
@@ -85,11 +85,12 @@ var intervalId;
 var running = false;
 var pick;
 
-
 $('.timer').hide()
+
 //click start button to start game
 $(".start").on("click", function () {
-		$(".start").hide();
+        $(".start").hide();
+        $('.timer').show()
 		runTimer();
 })
 	
@@ -105,8 +106,8 @@ function decrement() {
 	$(".timer").html("<h3>Time remaining: " + timer + "</h3>");
 	timer --;
 
-	//stop timer if reach 0
-	if (timer === 0) {
+	//stop timer if reach -1
+	if (timer === -1) {
 		unanswerCount++;
 		stop();
 	}	
@@ -117,5 +118,18 @@ function stop() {
 	clearInterval(intervalId);
 }
 
+function displayQuestion(){
+    index= Math.floor(Math.random()*options.length+1);
+    pick = options[index];
+    console.log(pick)
+    $(".question").html("<h2>" + pick.question + "</h2>");
+    $(".answer1").html(pick.choice[0]);
+    $(".answer2").html(pick.choice[1]);
+    $(".answer3").html(pick.choice[2]);
+    $(".answer4").html(pick.choice[3]);
+}
+
+displayQuestion() 
+console.log(index)
 
 });
