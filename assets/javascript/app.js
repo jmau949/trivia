b58c082e8fc019a053e27dd3beb978163f3a05a0
D Jonathan Mau <jmau@ucdavis.edu>
2019-07-19 16:49:35 -0500

    'edits'

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,124 +12,177 @@ after last question show results. and retry button
 
 
 
-$(document).ready(function() {
-var options = [
-    {
-        question: "What is the name of the large red ball used in Quidditch?",
-        choice: ["Quiddle", "Snitch", "Sniffle", "Quaffle"],
-        answer: 3,
-        photo: "assets/images/quaffle.jpg",
-    },
-    {
-        question: "What is the name of the goblin who assists Harry, Ron, and Hermione in infiltrating Gringotts?",
-        choice: ["Gornuk", "Golgomath", "Griphook", "Gregorovitch"],
-        answer: 2,
-        photo: "assets/images/griphook.jpg",
-    },
-    {
-        question: "Which of the following dragons was NOT used in the Triwizard Tournament in 1994?",
-        choice: ["Hungarian Horntail", "Swedish Short-Snout", "Norwegian Ridgeback", "Chinese Fireball"],
-        answer: 3,
-        photo: "assets/images/ridgeback.png",
-    },
-    {
-        question: "What is the name of the Weasley family's elderly owl?",
-        choice: ["Elphias", "Errol", "Ernie", "Eldred"],
-        answer: 1,
-        photo: "assets/images/errol.png",
-    },
-    {
-        question: "Which of the Weasleys went to Romania to study dragons?",
-        choice: ["George", "Bill", "Charlie", "Percy"],
-        answer: 2,
-        photo: "assets/images/charlie.png",
+$(document).ready(function () {
+    var options = [
+        {
+            question: "What is the name of the large red ball used in Quidditch?",
+            choice: ["Quiddle", "Snitch", "Sniffle", "Quaffle"],
+            answer: 3,
+            photo: "assets/images/quaffle.jpg",
+        },
+        {
+            question: "What is the name of the goblin who assists Harry, Ron, and Hermione in infiltrating Gringotts?",
+            choice: ["Gornuk", "Golgomath", "Griphook", "Gregorovitch"],
+            answer: 2,
+            photo: "assets/images/griphook.jpg",
+        },
+        {
+            question: "Which of the following dragons was NOT used in the Triwizard Tournament in 1994?",
+            choice: ["Hungarian Horntail", "Swedish Short-Snout", "Norwegian Ridgeback", "Chinese Fireball"],
+            answer: 3,
+            photo: "assets/images/ridgeback.png",
+        },
+        {
+            question: "What is the name of the Weasley family's elderly owl?",
+            choice: ["Elphias", "Errol", "Ernie", "Eldred"],
+            answer: 1,
+            photo: "assets/images/errol.jpg",
+        },
+        {
+            question: "Which of the Weasleys went to Romania to study dragons?",
+            choice: ["George", "Bill", "Charlie", "Percy"],
+            answer: 2,
+            photo: "assets/images/charlie.jpg",
 
-    },
-    {
-        question: "Which is the house ghost of Hufflepuff?",
-        choice: ["The Fat Friar", "The Grey Lady", "The Bloody Baron", "Nearly Headless Nick"],
-        answer: 0,
-        photo: "assets/images/friar.jpg",
-    },
-    {
-        question: "Who was NOT a member of the 'Slug Club'?",
-        choice: ["Cormac Mclaggen", "Hermione Granger", "Ron Weasley", "Harry Potter"],
-        answer: 2,
-        photo: "assets/images/ronald.png",
-    },
-    {
-        question: "What kind of creature does Lee Jordan put in Professor Umbridge's office during the latter's reign as Hogwarts High Inquisitor?",
-        choice: ["Norwegian Ridgeback", "Niffler", "Nargle", "Nymph"],
-        answer: 1,
-        photo: "assets/images/niffler.png",
-    },
-    {
-        question: "What profession are Hermione Granger's Muggle parents?",
-        choice: ["Chefs", "Dentists", "Opticians", "Artists"],
-        answer: 2,
-        photo: "assets/images/parents.jpg",
-    },
-    {
-        question: "In which Quidditch position did Oliver Wood play when he was still at Hogwarts?",
-        choice: ["Chaser", "Beater", "Keeper", "Seeker"],
-        answer: 3,
-        photo: "assets/images/oliver.jpg",
-    },
-]
+        },
+        {
+            question: "Which is the house ghost of Hufflepuff?",
+            choice: ["The Fat Friar", "The Grey Lady", "The Bloody Baron", "Nearly Headless Nick"],
+            answer: 0,
+            photo: "assets/images/friar.jpg",
+        },
+        {
+            question: "Who was NOT a member of the 'Slug Club'?",
+            choice: ["Cormac Mclaggen", "Hermione Granger", "Ron Weasley", "Harry Potter"],
+            answer: 2,
+            photo: "assets/images/ronald.png",
+        },
+        {
+            question: "What kind of creature does Lee Jordan put in Professor Umbridge's office during the latter's reign as Hogwarts High Inquisitor?",
+            choice: ["Norwegian Ridgeback", "Niffler", "Nargle", "Nymph"],
+            answer: 1,
+            photo: "assets/images/niffler.jpg",
+        },
+        {
+            question: "What profession are Hermione Granger's Muggle parents?",
+            choice: ["Chefs", "Dentists", "Opticians", "Artists"],
+            answer: 1,
+            photo: "assets/images/parents.jpg",
+        },
+        {
+            question: "In which Quidditch position did Oliver Wood play when he was still at Hogwarts?",
+            choice: ["Chaser", "Beater", "Keeper", "Seeker"],
+            answer: 2,
+            photo: "assets/images/oliver.jpg",
+        },
+    ]
 
-var correctCount = 0;
-var wrongCount = 0;
-var unanswerCount = 0;
-var timer = 20;
-var intervalId;
-var running = false;
-var pick;
+    var correctCount = 0;
+    var wrongCount = 0;
+    var unanswerCount = 0;
+    var timer = 20;
+    var intervalId;
+    var userGuess = "";
+    var running = false;
+    var allCount = options.length;
+    var pick;
+    var index;
+    var newArray = [];
+    var holder = [];
 
-$('.timer').hide()
 
-//click start button to start game
-$(".start").on("click", function () {
+
+    $(".reset").hide();
+    $(".start").on("click", function () {
         $(".start").hide();
-        $('.timer').show()
-		runTimer();
+        displayQuestion();
+        runTimer();
+        for (var i = 0; i < options.length; i++) {
+            holder.push(options[i]);
+        }
+    })
+    function runTimer() {
+        if (!running) {
+            intervalId = setInterval(decrement, 1000);
+            running = true;
+        }
+    }
+    function decrement() {
+        $(".timeleft").html("<h3>Time remaining: " + timer + "</h3>");
+        timer--;
+        if (timer === 0) {
+            unanswerCount++;
+            stop();
+            $(".answerblock").html("<p>Time is up! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+            hidepicture();
+        }
+    }
+    function stop() {
+        running = false;
+        clearInterval(intervalId);
+    }
+    function displayQuestion() {
+        index = Math.floor(Math.random() * options.length);
+        pick = options[index];
+        $(".questionblock").html("<h2>" + pick.question + "</h2>");
+        for (var i = 0; i < pick.choice.length; i++) {
+            var userChoice = $("<div>");
+            userChoice.addClass("answerchoice");
+            userChoice.html(pick.choice[i]);
+            userChoice.attr("data-guessvalue", i);
+            $(".answerblock").append(userChoice);
+        }
+        $(".answerchoice").on("click", function () {
+            userGuess = parseInt($(this).attr("data-guessvalue"));
+            if (userGuess === pick.answer) {
+                stop();
+                correctCount++;
+                userGuess = "";
+                $(".answerblock").html("<p>Correct!</p>");
+                hidepicture();
+            } else {
+                stop();
+                wrongCount++;
+                userGuess = "";
+                $(".answerblock").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+                hidepicture();
+            }
+        })
+    }
+
+    function hidepicture() {
+        $(".answerblock").append("<img src=" + pick.photo + ">");
+        newArray.push(pick);
+        setTimeout(function () {
+            $(".answerblock").empty();
+            timer = 20;
+            if ((wrongCount + correctCount + unanswerCount) === allCount) {
+                $(".questionblock").empty();
+                $(".questionblock").html("<h3>Game Over!  Here's how you did: </h3>");
+                $(".answerblock").append("<h4> Correct: " + correctCount + "</h4>");
+                $(".answerblock").append("<h4> Incorrect: " + wrongCount + "</h4>");
+                $(".answerblock").append("<h4> Unanswered: " + unanswerCount + "</h4>");
+                $(".reset").show();
+                correctCount = 0;
+                wrongCount = 0;
+                unanswerCount = 0;
+            } else {
+                runTimer();
+                displayQuestion();
+            }
+        }, 2000);
+    }
+
+    $(".reset").on("click", function () {
+        $(".reset").hide();
+        $(".answerblock").empty();
+        $(".questionblock").empty();
+        for (var i = 0; i < holder.length; i++) {
+            options.push(holder[i]);
+        }
+        runTimer();
+        displayQuestion();
+
+    })
+
 })
-	
-//timer start
-function runTimer(){
-	if (!running) {
-	intervalId = setInterval(decrement, 1000); 
-	running = true;
-	}
-}
-//timer countdown
-function decrement() {
-	$(".timer").html("<h3>Time remaining: " + timer + "</h3>");
-	timer --;
-
-	//stop timer if reach -1
-	if (timer === -1) {
-		unanswerCount++;
-		stop();
-	}	
-}
-//timer stop
-function stop() {
-	running = false;
-	clearInterval(intervalId);
-}
-
-function displayQuestion(){
-    index= Math.floor(Math.random()*options.length+1);
-    pick = options[index];
-    console.log(pick)
-    $(".question").html("<h2>" + pick.question + "</h2>");
-    $(".answer1").html(pick.choice[0]);
-    $(".answer2").html(pick.choice[1]);
-    $(".answer3").html(pick.choice[2]);
-    $(".answer4").html(pick.choice[3]);
-}
-
-displayQuestion() 
-console.log(index)
-
-});
